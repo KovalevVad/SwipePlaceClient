72a4eae703aaed7e9bc6a5afaca76628a5fe024e
KaRToSHoW <miha.frolow2017@yandex.ru>
2024-05-12 23:37:13 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,23 +6,23 @@ const list = ["Лента", "Мессенджер", "Избранное", "Ли�
 const urls = ["/", "/", "/", "/", "/", "/"];
 
 export default function Header() {
-  return (
-    <header>
-      <a href="/"><img src={LOGO} alt="LOGO" className="logo" /></a>
-      <nav>
-        <ul className="header__nav-ul">
-          {
-            list.map((item, index) => {
-              return (
-                <li key={index}>
-                  <a href={urls[index]}>{item}</a>
-                </li>
-              );
-            })
-          }
-        </ul>
-      </nav>
-      <button className="header__button">Войти</button>
-    </header>
-  );
+    return (
+        <header>
+            <a href="/"><img src={LOGO} alt="LOGO" className="logo" /></a>
+            <nav>
+                <ul className="header__nav-ul">
+                    {
+                        list.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a href={urls[index]}>{item}</a>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </nav>
+            <button className="header__button">Войти</button>
+        </header>
+    );
 }
