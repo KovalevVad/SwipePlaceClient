65ba68e7933cf5080cab6ee1ea04fd3aa2782607
Mikhail Frolov <145573071+KaRToSHoW@users.noreply.github.com>
2024-05-12 23:38:51 +0300

    fix

---
+++ header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./header.css";
+import LOGO from "../../img/logo-header.png";
+
+const list = ["Лента", "Мессенджер", "Избранное", "Личный кабинет", "Выбор валюты", 'Зарегистрироваться'];
+const urls = ["/", "/", "/", "/", "/", "/"];
+
+export default function Header() {
+    return (
+        <header>
+            <a href="/"><img src={LOGO} alt="LOGO" className="logo" /></a>
+            <nav>
+                <ul className="header__nav-ul">
+                    {
+                        list.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a href={urls[index]}>{item}</a>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </nav>
+            <button className="header__button">Войти</button>
+        </header>
+    );
+}
